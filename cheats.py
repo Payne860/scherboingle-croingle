296172804232fca8512c05ff5089c42ae59f6d93
Payne860 <88009614+Payne860@users.noreply.github.com>
2023-01-06 15:32:51 -0800

    added cheat center

---
+++ cheats.py
@@ -0,0 +1,12 @@
+print("welcome to the scherboingle croingle cheat center.")
+import time
+time.sleep(0.3)
+print("would you like to see death endings or other endings?")
+ending = input("MAKE SELECTION: ")
+if ending == "death" then:
+    print("the death endings are getting killed by a bear and getting killed by king monkey.")
+elif ending == "other" then:
+    print("the other ending is staying put.")
+else:
+    print("that was not an option. please restart the program and use only lower case.")
+exit()
