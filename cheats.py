5bb6127a10ed71fe743eb9db42a27ee26dd21014
Payne860 <88009614+Payne860@users.noreply.github.com>
2023-01-06 15:38:17 -0800

    fixed last commit

--- cheats.py
+++ cheats.py
@@ -3,9 +3,9 @@ import time
 time.sleep(0.3)
 print("would you like to see death endings or other endings?")
 ending = input("MAKE SELECTION: ")
-if ending == "death" then:
+if ending == "death":
     print("the death endings are getting killed by a bear and getting killed by king monkey.")
-elif ending == "other" then:
+elif ending == "other":
     print("the other ending is staying put.")
 else:
     print("that was not an option. please restart the program and use only lower case.")
