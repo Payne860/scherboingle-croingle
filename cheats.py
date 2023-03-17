d112536c9d581f61337fa285791961202879f3db
Payne860 <88009614+Payne860@users.noreply.github.com>
2023-03-17 08:54:24 -0700

    bdetter cheats

--- cheats.py
+++ cheats.py
@@ -1,12 +1,20 @@
-print("welcome to the scherboingle croingle cheat center.")
 import time
-time.sleep(0.3)
-print("would you like to see death endings or other endings?")
-ending = input("MAKE SELECTION: ")
-if ending == "death":
-    print("the death endings are getting killed by a bear and getting killed by king monkey.")
-elif ending == "other":
-    print("the other ending is staying put.")
-else:
-    print("that was not an option. please restart the program and use only lower case.")
-exit()
+def cntr()
+    print("welcome to the scherboingle croingle cheat center.")
+    print("would you like to see death endings or other endings?")
+    ending = input("MAKE SELECTION: ")
+    if ending == "death":
+        print("the death endings are getting killed by a bear and getting killed by king monkey.")
+        print("thank you")
+        exit()
+    elif ending == "other":
+        print("the other ending is staying put.")
+        print("thank you.")
+        exit()
+    else:
+        print("that was not an option. the program is restarting. please use all lowercase.")
+cntr()
+cntr()
+cntr()
+print("too many failed attempts. please restart the program.")
+print("thank you for using oMy software")
